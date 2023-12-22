e8545562955ded58c6ad563c2bb5d40c0728b89a
FelipeBrocca <pipebrocca@gmail.com>
2023-12-22 03:53:32 -0300

    allow origin deployed front

--- database/config/allowOrigins.js
+++ database/config/allowOrigins.js
@@ -1,6 +1,8 @@
 export const allowOrigins = [
+    // prod
+    process.env.URL_REACT
     //dev
-    'http://localhost:3000',
-    'http://localhost:5000',
-    'http://localhost:9001',
+    // 'http://localhost:3000',
+    // 'http://localhost:5000',
+    // 'http://localhost:9001',
 ]
