4e33c940a55d90c2d6a5286b6912e61db3aa4883
FelipeBrocca <pipebrocca@gmail.com>
2023-12-31 15:27:04 -0300

    allow origin

--- database/config/allowOrigins.js
+++ database/config/allowOrigins.js
@@ -1,10 +1,11 @@
 export const allowOrigins = [
     'https://panel-unicred.vercel.app',
     //DEV
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'http://localhost:9001',
 ]
 // prod
 //dev
-// 'http://localhost:9001',
+// 
 // 'http://localhost:5000',
 
