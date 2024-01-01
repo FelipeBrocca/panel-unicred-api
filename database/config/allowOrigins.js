50754efbae5db17aebe844a15e6616e42797cd15
FelipeBrocca <pipebrocca@gmail.com>
2024-01-01 20:53:59 -0300

    allow unicred site

--- database/config/allowOrigins.js
+++ database/config/allowOrigins.js
@@ -1,11 +1,12 @@
 export const allowOrigins = [
     'https://panel-unicred.vercel.app',
+    'https://www.unicred.com.ar/'
     //DEV
-    'http://localhost:3000',
-    'http://localhost:9001',
 ]
 // prod
 //dev
 // 
+// 'http://localhost:3000',
+// 'http://localhost:9001',
 // 'http://localhost:5000',
 
