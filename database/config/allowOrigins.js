0eb8412c5d6d3ee33ef913df27f1a9238e15ecd6
FelipeBrocca <pipebrocca@gmail.com>
2024-01-02 15:07:14 -0300

    allow dev mode

--- database/config/allowOrigins.js
+++ database/config/allowOrigins.js
@@ -1,12 +1,12 @@
 export const allowOrigins = [
     'https://panel-unicred.vercel.app',
-    'https://www.unicred.com.ar'
+    'https://www.unicred.com.ar',
+    'http://localhost:9001'
     //DEV
 ]
 // prod
 //dev
 // 
 // 'http://localhost:3000',
-// 'http://localhost:9001',
 // 'http://localhost:5000',
 
